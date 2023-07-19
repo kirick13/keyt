a6ccfe30b19d0bde9f3eae2d423987b635960c68
Daniil Kirichenko <daniil@kirick.me>
2023-07-19 10:07:59 +0300

    Names regexp updated

--- src/consts.js
+++ src/consts.js
@@ -2,8 +2,8 @@
 import { DAEMON_SET_RESOURCE_TYPE } from './types/daemon-set.js';
 import { DEPLOYMENT_RESOURCE_TYPE } from './types/deployment.js';
 
-export const DASHED_WORD_REGEXP = /^[a-z](?:[\w-]*[\da-z])?$/;
-export const NAME_REGEXP = /^[a-z](?:[\da-z-]*[\da-z])?$/;
+export const DASHED_WORD_REGEXP = /^[\da-z](?:[\w-]*[\da-z])?$/;
+export const NAME_REGEXP = /^[\da-z](?:[\da-z-]*[\da-z])?$/;
 export const FQDN_REGEXP = /^[\da-z](?:[\d_a-z-]*[\da-z])?(?:\.[\da-z](?:[\d_a-z-]*[\da-z])?)*$/;
 export const FQDN_WILDCARD_REGEXP = /^(?:\*\.)?[\da-z](?:[\d_a-z-]*[\da-z])?(?:\.[\da-z](?:[\d_a-z-]*[\da-z])?)*$/;
 export const ENV_KEY_REGEXP = /^[A-Z][\dA-Z_]*$/;
