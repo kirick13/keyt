295c9a0a4ef543e442f6df8be34ae90b1bac0586
Daniil Kirichenko <daniil@kirick.me>
2023-11-30 14:54:16 +0300

    [v0.1.14-dev] Env variables K8S_HOST, K8S_PORT

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 DOCKER_IMAGE='kirickme/keyt'
-DOCKER_TAG='0.1.12-dev'
+DOCKER_TAG='0.1.14-dev'
 
 docker manifest inspect $DOCKER_IMAGE:$DOCKER_TAG >/dev/null 2>&1
 if [ $? -ne 0 ]; then
